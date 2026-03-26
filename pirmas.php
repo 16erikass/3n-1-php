f475a8075e5f2cff7ff7155b8247f12c5d176bf8
16erikass <16erikass@gmail.com>
2026-03-26 11:30:05 +0200

    pakeisti komentarai
    Komentaru pakeitimas

--- pirmas.php
+++ pirmas.php
@@ -5,7 +5,9 @@ $iteracijos = 0;
 
 echo "Pradinis skaičius: $originalus_sk <br>";
 echo "Seka: ";
-// vykdomas ciklas iki 1
+
+//Ciklas veiks tol, kol skaičius taps 1
+
 while ($n != 1) {
 
 // spausdina skaiciu
@@ -13,9 +15,13 @@ while ($n != 1) {
 
     if ($n % 2 == 0) { // Lyginis
 
+     //Jei lyginis dalinama iš 2
+
     $n = $n / 2;
 
     } else { // Nelyginis
+
+        // Jei nelyginis  padauginama iš 3 ir pridedamas 1
 
     $n = ($n * 3 ) + 1;
 
