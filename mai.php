5f2d0191dbc37aa5ba4ecd899afebe8f77d83807
16erikass <16erikass@gmail.com>
2026-03-26 11:51:12 +0200

    komentaru patobulinimai
    komentaru patobulinimai

--- mai.php
+++ mai.php
@@ -4,19 +4,31 @@ require 'collat.php';
 //Sukuriamas objektas iš vaikines klases
 $objektas = new CollatzPapildyta();
 
-//reikia pakeisti nuo ir iki jei nori kito intervalo
+// Nustatomas intervalas (nuo ir iki)
+// Jei nori kito intervalo reikia pakeisti šias reikšmes
 $nuo = 1024;
 $iki = 4500;
 
+// Gaunami statistikos rezultatai pasirinktame intervale
 $rezultatai = $objektas->gautiStatistika($nuo, $iki);
 
 //isvedamas rezultatas
-echo "Autorius: " . CollatzPapildyta::AUTORIUS . "<br>";
-echo "Versija: " . CollatzPapildyta::VERSIJA . "<br> ";
+echo "Autorius: " . CollatzPapildyta::AUTORIUS . "<br>";//Išvedamas klasėje apibrėžtas autorius (konstanta)
+echo "Versija: " . CollatzPapildyta::VERSIJA . "<br> ";//Išvedama programos versija (konstanta)
+
+//Parodomas skaičius, kuriam prireikė daugiausia iteracijų
 echo "Skaičius su daugiausia iteracijų: <b>" . $rezultatai['daugiausia_it']['sk'] . "</b> (" . $rezultatai['daugiausia_it']['kiekis'] . ")<br>";
+
+//Parodomas skaičius, kuriam prireikė mažiausiai iteracijų
 echo "Skaičius su mažiausia iteracijų: <b>" . $rezultatai['maziausia_it']['sk'] . "</b> (" . $rezultatai['maziausia_it']['kiekis'] . ")<br>";
+
+//Parodomas skaičius, kuris pasiekė didžiausią reikšmę sekoje
 echo "Skaičius su didžiausia pasiekta reikšme: <b>" . $rezultatai['didziausia_re']['sk'] . "</b> (pasiekė " . $rezultatai['didziausia_re']['reiksme'] . ")<br>";
 
-//Isvedama nauja histograma
+
+
+//Sudaroma histograma pagal pasirinktą intervalą
 $objektas->sudarytiHistograma($nuo, $iki);
+
+//Išvedama histograma į ekraną
 $objektas->isvestiHistograma();
--- main.php
+++ main.php
@@ -1,11 +1,18 @@
 <?php
+//Įtraukiamas failas, kuriame yra Collatz klasė
 require 'Collatz.php';
-
+//Sukuriamas naujas Collatz klasės objektas
 $objektas = new Collatz();
+//Iškviečiama funkcija, kuri apskaičiuoja statistiką intervale nuo 700 iki 45044
 $rezultatai = $objektas->gautiStatistika(700, 45044); //reikia pakeisti jei nori kito intervalo
 
 //isvedamas rezultatas
 
+//Parodomas skaičius, kuriam prireikė daugiausia iteracijų, kol pasiekė 1
 echo "Skaičius su daugiausia iteracijų: <b>" . $rezultatai['daugiausia_it']['sk'] . "</b> (" . $rezultatai['daugiausia_it']['kiekis'] . ")<br>";
+
+//Parodomas skaičius, kuriam prireikė mažiausiai iteracijų
 echo "Skaičius su mažiausia iteracijų: <b>" . $rezultatai['maziausia_it']['sk'] . "</b> (" . $rezultatai['maziausia_it']['kiekis'] . ")<br>";
+
+//Parodomas skaičius, kuris pasiekė didžiausią reikšmę Collatz sekoje
 echo "Skaičius su didžiausia pasiekta reikšme: <b>" . $rezultatai['didziausia_re']['sk'] . "</b> (pasiekė " . $rezultatai['didziausia_re']['reiksme'] . ")<br>";
